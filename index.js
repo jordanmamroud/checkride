0cb2c1564216e9f0cc101ac3cf37f607ea3d0440
joshmamroud <josh.mamroud@gmail.com>
2016-09-20 21:03:04 -0400

    Added express

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/'));
 
 // views is directory for all template files
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
 
-//app.get('/', function(request, response) {
-//  response.render('pages/index');
-//});
+app.get('/', function(request, response) {
+ response.render('index');
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
